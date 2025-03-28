01ca7dabde248843232b91c6fb4536277befa0db
teamd <teamdlok@gmail.com>
2025-03-28 15:03:18 +1000

    React base template

---
+++ react/react-drf-note-app/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Filter from '../components/Filter'
+import NoteCardContainer from '../components/NoteCardContainer'
+
+const HomePage = () => {
+  return (
+    <>
+    <Filter />
+    <NoteCardContainer />
+    </>
+  )
+}
+
+export default HomePage
